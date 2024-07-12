2b8965613c7b614d537890b7349994e9ad5a1668
Ben Campion <bencampion@users.noreply.github.com>
2024-07-12 16:40:58 +0100

    Apply prettier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { countVotes } from './results.js';
+import { useState } from "react";
+import { countVotes } from "./results.js";
 
 const partyNames = {
   apni: <abbr title="Alliance Party of Northern Ireland">APNI</abbr>,
@@ -9,32 +9,35 @@ const partyNames = {
   green: <abbr title="Green Party">Green</abbr>,
   lab: <abbr title="Labour Party">Labour</abbr>,
   ld: <abbr title="Liberal Democrats">Lib Dems</abbr>,
-  pc: 'Plaid Cymru',
+  pc: "Plaid Cymru",
   sdlp: <abbr title="Social Democratic and Labour Party">SDLP</abbr>,
-  sf: 'Sinn Féin',
+  sf: "Sinn Féin",
   snp: <abbr title="Scottish National Party">SNP</abbr>,
   uup: <abbr title="Ulster Unionist Party">UUP</abbr>,
-  other: 'Other',
-  left: 'Left Alliance',
-  right: 'Right Alliance'
+  other: "Other",
+  left: "Left Alliance",
+  right: "Right Alliance",
 };
 
 function useSearchParam(name) {
   const searchParams = new URLSearchParams(window.location.search);
   const [values, setValues] = useState(searchParams.getAll(name));
-  return [values, newValues => {
-    setValues(newValues);
-    const url = new URL(window.location);
-    url.searchParams.delete(name);
-    newValues.forEach(value => url.searchParams.append(name, value));
-    url.searchParams.sort();
-    window.history.replaceState({}, '', url);
-  }];
+  return [
+    values,
+    (newValues) => {
+      setValues(newValues);
+      const url = new URL(window.location);
+      url.searchParams.delete(name);
+      newValues.forEach((value) => url.searchParams.append(name, value));
+      url.searchParams.sort();
+      window.history.replaceState({}, "", url);
+    },
+  ];
 }
 
 function App() {
-  const [left, setLeft] = useSearchParam('left');
-  const [right, setRight] = useSearchParam('right');
+  const [left, setLeft] = useSearchParam("left");
+  const [right, setRight] = useSearchParam("right");
 
   const alliances = { left, right };
   const results = countVotes(alliances);
@@ -42,15 +45,35 @@ function App() {
   return (
     <div className="container">
       <header className="m-5">
-        <h1 className="title mb-3">2019 UK general election alliance calculator</h1>
-        <p className="subtitle mt-0">There were <strong>229</strong> constituencies in the <a href="https://en.wikipedia.org/wiki/2019_United_Kingdom_general_election">2019 United Kingdom general election</a> where the winner did not get a majority of the votes. What would have happened if parties had formed alliances to combine their votes behind a single candidate in each of those constituencies?</p>
+        <h1 className="title mb-3">
+          2019 UK general election alliance calculator
+        </h1>
+        <p className="subtitle mt-0">
+          There were <strong>229</strong> constituencies in the{" "}
+          <a href="https://en.wikipedia.org/wiki/2019_United_Kingdom_general_election">
+            2019 United Kingdom general election
+          </a>{" "}
+          where the winner did not get a majority of the votes. What would have
+          happened if parties had formed alliances to combine their votes behind
+          a single candidate in each of those constituencies?
+        </p>
       </header>
 
       <Results results={results} alliances={alliances} />
 
       <section className="columns">
-        <Alliance name="Left" members={left} setMembers={setLeft} opponents={right} />
-        <Alliance name="Right" members={right} setMembers={setRight} opponents={left} />
+        <Alliance
+          name="Left"
+          members={left}
+          setMembers={setLeft}
+          opponents={right}
+        />
+        <Alliance
+          name="Right"
+          members={right}
+          setMembers={setRight}
+          opponents={left}
+        />
       </section>
 
       <SeatChanges results={results} />
@@ -58,8 +81,18 @@ function App() {
       <hr />
 
       <footer className="block is-size-7 m-5">
-        <p>Data: <a href="https://commonslibrary.parliament.uk/research-briefings/cbp-8749/">General Election 2019: full results and analysis</a></p>
-        <p>Code: <a href="https://github.com/bencampion/election-alliance-calculator">https://github.com/bencampion/election-alliance-calculator</a></p>
+        <p>
+          Data:{" "}
+          <a href="https://commonslibrary.parliament.uk/research-briefings/cbp-8749/">
+            General Election 2019: full results and analysis
+          </a>
+        </p>
+        <p>
+          Code:{" "}
+          <a href="https://github.com/bencampion/election-alliance-calculator">
+            https://github.com/bencampion/election-alliance-calculator
+          </a>
+        </p>
       </footer>
     </div>
   );
@@ -68,16 +101,21 @@ function App() {
 function Results({ results, alliances }) {
   return (
     <section className="block mx-5">
-      <div className="field is-grouped is-grouped-centered is-grouped-multiline" role="table">
+      <div
+        className="field is-grouped is-grouped-centered is-grouped-multiline"
+        role="table"
+      >
         {Object.entries(results.seats)
           .filter(([party, count]) => count > 0)
           .sort((a, b) => b[1] - a[1])
-          .map(([party, count]) => <Votes
-            key={party}
-            party={party}
-            count={count}
-            alliances={alliances}
-          />)}
+          .map(([party, count]) => (
+            <Votes
+              key={party}
+              party={party}
+              count={count}
+              alliances={alliances}
+            />
+          ))}
       </div>
       <p className="has-text-centered">
         <strong>326</strong> seats needed for a majority
@@ -89,36 +127,52 @@ function Results({ results, alliances }) {
 function Votes({ party, count, alliances }) {
   return (
     <div className="control" role="row">
-      <div className="tags has-addons mb-0 is-justify-content-center" role="rowheader">
-        {(alliances[party] ?? [party]).map(party => <span key={party} className={`tag is-${party}`} />)}
+      <div
+        className="tags has-addons mb-0 is-justify-content-center"
+        role="rowheader"
+      >
+        {(alliances[party] ?? [party]).map((party) => (
+          <span key={party} className={`tag is-${party}`} />
+        ))}
         <span className="tag is-dark">{partyNames[party]}</span>
       </div>
-      <div className="title has-text-centered" role="cell">{count}</div>
+      <div className="title has-text-centered" role="cell">
+        {count}
+      </div>
     </div>
   );
 }
 
 function Alliance({ name, members, setMembers, opponents }) {
-  const toggleMembership = e => {
+  const toggleMembership = (e) => {
     const party = e.target.value;
-    setMembers(members.includes(party) ? members.filter(m => m !== party) : [...members, party]);
+    setMembers(
+      members.includes(party)
+        ? members.filter((m) => m !== party)
+        : [...members, party],
+    );
   };
-  const parties = Object.keys(partyNames).filter(party => party !== 'left' && party !== 'right');
+  const parties = Object.keys(partyNames).filter(
+    (party) => party !== "left" && party !== "right",
+  );
   return (
     <div>
       <h2 className="title is-4 mb-1">{name} Alliance</h2>
       <fieldset>
         <legend className="mb-3">
-          Select parties to combine their votes to form a <strong>{name} Alliance</strong>.
+          Select parties to combine their votes to form a{" "}
+          <strong>{name} Alliance</strong>.
         </legend>
         <div className="field is-grouped is-grouped-multiline">
-          {parties.map(party => <PartyCheckBox
-            key={party}
-            party={party}
-            onChange={toggleMembership}
-            checked={members.includes(party)}
-            disabled={opponents.includes(party)}
-          />)}
+          {parties.map((party) => (
+            <PartyCheckBox
+              key={party}
+              party={party}
+              onChange={toggleMembership}
+              checked={members.includes(party)}
+              disabled={opponents.includes(party)}
+            />
+          ))}
         </div>
       </fieldset>
     </div>
@@ -128,10 +182,10 @@ function Alliance({ name, members, setMembers, opponents }) {
 function PartyCheckBox({ party, onChange, checked, disabled }) {
   return (
     <div className="control">
-      <label className={`tags has-addons ${disabled ? '' : 'is-clickable'}`}>
+      <label className={`tags has-addons ${disabled ? "" : "is-clickable"}`}>
         <span className={`tag is-${party}`}>
           <input
-            className={disabled ? undefined : 'is-clickable'}
+            className={disabled ? undefined : "is-clickable"}
             type="checkbox"
             disabled={disabled}
             value={party}
@@ -139,7 +193,13 @@ function PartyCheckBox({ party, onChange, checked, disabled }) {
             checked={checked}
           />
         </span>
-        <span className={`tag ${disabled ? 'has-background-grey-dark has-text-grey-light' : 'is-dark'}`}>
+        <span
+          className={`tag ${
+            disabled
+              ? "has-background-grey-dark has-text-grey-light"
+              : "is-dark"
+          }`}
+        >
           {partyNames[party]}
         </span>
       </label>
@@ -151,18 +211,22 @@ function SeatChanges({ results }) {
   return (
     <section className="mx-5 content">
       <details>
-        <summary className="title is-5">Seats changed ({results.changes.length})</summary>
-        {results.changes.length === 0 ?
+        <summary className="title is-5">
+          Seats changed ({results.changes.length})
+        </summary>
+        {results.changes.length === 0 ? (
           <p>No seat changes.</p>
-          :
+        ) : (
           <ul>
-            {results.changes.map(result => (
+            {results.changes.map((result) => (
               <li key={result.name}>
-                {result.name} — {result.region} (<strong>{partyNames[result.allianceWinner]}</strong> gain from <strong>{partyNames[result.winner]}</strong>)
+                {result.name} — {result.region} (
+                <strong>{partyNames[result.allianceWinner]}</strong> gain from{" "}
+                <strong>{partyNames[result.winner]}</strong>)
               </li>
             ))}
           </ul>
-        }
+        )}
       </details>
     </section>
   );
--- src/results.js
+++ src/results.js
@@ -1,9 +1,9 @@
-import { majoritySeats, results } from './data.js';
+import { majoritySeats, results } from "./data.js";
 
 function initialSeats(alliances) {
   const seats = Object.assign({}, majoritySeats);
   Object.entries(alliances).forEach(([name, parties]) => {
-    parties.forEach(party => {
+    parties.forEach((party) => {
       seats[name] = (seats[name] ?? 0) + seats[party];
       delete seats[party];
     });
@@ -15,10 +15,18 @@ function countVotes(alliances) {
   const seats = initialSeats(alliances);
   const changes = [];
   for (const result of results) {
-    const bestAlliance = Object.entries(alliances).reduce((currentWinner, [name, parties]) => {
-      const votes = parties.reduce((votes, party) => (result[party] ?? 0) + votes, 0);
-      return votes > currentWinner.votes ? { name, votes, parties } : currentWinner;
-    }, { votes: 0 });
+    const bestAlliance = Object.entries(alliances).reduce(
+      (currentWinner, [name, parties]) => {
+        const votes = parties.reduce(
+          (votes, party) => (result[party] ?? 0) + votes,
+          0,
+        );
+        return votes > currentWinner.votes
+          ? { name, votes, parties }
+          : currentWinner;
+      },
+      { votes: 0 },
+    );
     if (bestAlliance.votes >= result[result.winner]) {
       seats[bestAlliance.name]++;
       if (!bestAlliance.parties.includes(result.winner)) {
